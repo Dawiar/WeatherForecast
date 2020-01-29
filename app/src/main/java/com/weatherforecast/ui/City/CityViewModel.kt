2212355b97a5526dd15497360f8cd41b3660ff84
Dawiar <Daviar@bigmir.net>
2020-01-30 00:12:55 +0300

    Initial commit

---
+++ app/src/main/java/com/weatherforecast/ui/City/CityViewModel.kt
@@ -0,0 +1,7 @@
+package com.weatherforecast.ui.City
+
+import androidx.lifecycle.ViewModel
+
+class CityViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/weatherforecast/ui/Weather/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.weatherforecast.ui.Weather
+
+import androidx.lifecycle.ViewModel
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
